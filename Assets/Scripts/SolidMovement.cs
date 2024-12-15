039822d1b62e21bfd9734526d53b47e924981dcf
Leonid <159773372+Leonidino@users.noreply.github.com>
2024-12-15 14:32:54 +0100

    Add Solid Condition
    Solid Movement + Ice cube asset + random background

---
+++ Assets/Scripts/SolidMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer; 
+
+    //private Animator animator;
+    private BoxCollider2D boxCollider;
+    private float horizontalInput;
+
+
+    private void Awake(){ 
+        body = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //stop rotation
+        body.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
+        
+        //Flip the player when moving left-right
+        if(horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if(horizontalInput < -0.01f)
+            transform.localScale =   new Vector3(-1,1,1);
+
+
+            body.linearVelocity = new Vector2(horizontalInput*speed, body.linearVelocity.y);
+
+            body.gravityScale = 1;
+
+    }
+
+
+    private bool isGrounded(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+}
