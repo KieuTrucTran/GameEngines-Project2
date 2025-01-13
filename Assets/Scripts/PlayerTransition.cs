420926dc24d5e0818dce9895713ffbdefeef9df0
Tom <tl.littmann@gmail.com>
2025-01-13 22:41:44 +0100

    added climate zone compatibility in Player Script

--- Assets/Scripts/PlayerTransition.cs
+++ Assets/Scripts/PlayerTransition.cs
@@ -11,6 +11,11 @@ public class PlayerTransition : MonoBehaviour
     Vector3 currentPosition = new Vector3(0, 0, 0);
     int currentStateIndex = 0;
 
+    bool inHeatZone = false;
+
+    bool solidDisabled = false;
+    bool gasDisabled = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,30 +32,40 @@ public class PlayerTransition : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            currentStateIndex = 0;
-
-            deactivateAll();
-            playerStates[0].gameObject.SetActive(true);
-            playerStates[0].position = currentPosition;
+            if(!solidDisabled) activateState(0);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            currentStateIndex = 1;
-
-            deactivateAll();
-            playerStates[1].gameObject.SetActive(true);
-            playerStates[1].position = currentPosition;
-            playerStates[2].gameObject.SetActive(true);
+            activateState(1);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            currentStateIndex = 3;
+            if(!gasDisabled) activateState(3); //skipped 2, which are the particles
+        }
+    }
 
-            deactivateAll();
-            playerStates[3].gameObject.SetActive(true);
-            playerStates[3].position = currentPosition;
+    private void activateState(int state)
+    {
+        currentStateIndex = state;
+        deactivateAll();
+
+        switch (state)
+        {
+            case 0:
+                playerStates[0].gameObject.SetActive(true);
+                playerStates[0].position = currentPosition;
+                break;
+            case 1:
+                playerStates[1].gameObject.SetActive(true);
+                playerStates[1].position = currentPosition;
+                playerStates[2].gameObject.SetActive(true);
+                break;
+            case 3:
+                playerStates[3].gameObject.SetActive(true);
+                playerStates[3].position = currentPosition;
+                break;
         }
     }
 
@@ -59,6 +74,34 @@ public class PlayerTransition : MonoBehaviour
         foreach (Transform t in playerStates)
         {
             t.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Heat")
+        {
+            solidDisabled = true;
+            if (currentStateIndex == 0) activateState(1);
+        }
+
+        if(collider.gameObject.tag == "Cold")
+        {
+            gasDisabled = true;
+            if (currentStateIndex == 3) activateState(1);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Heat")
+        {
+            solidDisabled = false;
+        }
+
+        if (collider.gameObject.tag == "Cold")
+        {
+            gasDisabled = false;
         }
     }
 }
