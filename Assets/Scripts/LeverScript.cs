84e99092d102f53b142c8fe872d45d85184ae610
Tom <tl.littmann@gmail.com>
2025-01-14 23:39:03 +0100

    tutorial level v1

---
+++ Assets/Scripts/LeverScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    public Sprite leverOff;
+    public Sprite leverOn;
+
+    public GameObject wallTop;
+    public GameObject wallBottom;
+
+    private bool playerInDistance = false;
+    private bool leverPressed = false;
+
+    private float initialPositionY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        initialPositionY = wallTop.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                leverPressed = true;
+                GetComponent<SpriteRenderer>().sprite = leverOn;
+            }
+        }    
+
+        if(leverPressed && wallTop.transform.position.y < initialPositionY + 3)
+        {
+            wallTop.transform.position += Vector3.up * Time.deltaTime;
+            wallBottom.transform.position += Vector3.down * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInDistance = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInDistance = false;
+        }
+    }
+}
