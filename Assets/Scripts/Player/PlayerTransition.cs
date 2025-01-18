bace73631da80df4e04fc2e4a1eecdc6b3013053
Leonid <159773372+Leonidino@users.noreply.github.com>
2025-01-18 18:45:53 +0100

    Thermometer and Hazards

--- Assets/Scripts/Player/PlayerTransition.cs
+++ Assets/Scripts/Player/PlayerTransition.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerTransition : MonoBehaviour
 {
@@ -16,10 +17,16 @@ public class PlayerTransition : MonoBehaviour
     bool solidDisabled = false;
     bool gasDisabled = false;
 
+    [SerializeField] private Image thermometer;
+    private Animator animator; 
+    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
+        animator = thermometer.GetComponent<Animator>();
+        animator.SetBool("goIdle", true);
     }
 
     // Update is called once per frame
@@ -46,6 +53,17 @@ public class PlayerTransition : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             if (!gasDisabled) activateState(3); //skipped 2, which are the particles
+        }
+
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0); // Get state info for the first layer
+        if (stateInfo.IsName("Thermometer_DeFreeze") && stateInfo.normalizedTime >= 1.0f)
+        {
+            animator.SetBool("goIdle", true);
+                //animator.ResetTrigger("freezing");
+        } else if (stateInfo.IsName("Thermometer_DeHeat") && stateInfo.normalizedTime >= 1.0f){
+            animator.SetBool("goIdle", true);
+                //animator.ResetTrigger("heating");
         }
     }
 
@@ -82,31 +100,44 @@ public class PlayerTransition : MonoBehaviour
 
     public void zoneEntered(Collider2D collider)
     {
+        
 
         if (collider.gameObject.tag == "Heat")
         {
+            animator.SetTrigger("heating");
+            animator.SetBool("goIdle",false);
+            //animator.ResetTrigger("deheating");
             Debug.Log("Heat entered");
-
+            
             solidDisabled = true;
             if (currentStateIndex == 0) activateState(1);
         }
 
         if (collider.gameObject.tag == "Cold")
         {
+            animator.SetTrigger("freezing");
+            animator.SetBool("goIdle",false);
+            //animator.ResetTrigger("defreezing");
+            
             gasDisabled = true;
             if (currentStateIndex == 3) activateState(1);
         }
     }
     public void zoneEnxited(Collider2D collider)
     {
+       
+
         if (collider.gameObject.tag == "Heat")
         {
+            animator.SetTrigger("deheating");
+            
             solidDisabled = false;
         }
 
         if (collider.gameObject.tag == "Cold")
         {
             gasDisabled = false;
+            animator.SetTrigger("defreezing");
         }
     }
 
